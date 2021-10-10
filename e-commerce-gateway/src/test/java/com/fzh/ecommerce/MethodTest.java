e0511080ad3d64cbcc3c54e2aba2565a9f87a806
DawnPeanut <1540680885@qq.com>
2021-10-10 17:31:45 +0800

    test(test方法): perdicate test
    测试Predicate的用法

--- e-commerce-gateway/src/test/java/com/fzh/ecommerce/MethodTest.java
+++ e-commerce-gateway/src/test/java/com/fzh/ecommerce/MethodTest.java
@@ -27,4 +27,67 @@ public class MethodTest {
         System.out.println(newStr);
 
     }
+
+    public static List<String> MICRO_SERVICE = Arrays.asList(
+            "nacos", "authority", "gateway", "ribbon", "feign", "hystrix", "e-commerce"
+    );
+
+    /**
+     * <h2>test 方法主要用于参数符不符合规则, 返回值是 boolean</h2>
+     * */
+    @Test
+    public void testPredicateTest() {
+
+        Predicate<String> letterLengthLimit = s -> s.length() > 5;
+        MICRO_SERVICE.stream().filter(letterLengthLimit).forEach(System.out::println);
+    }
+
+    /**
+     * <h2>and 方法等同于我们的逻辑与 &&, 存在短路特性, 需要所有的条件都满足</h2>
+     * */
+    @Test
+    public void testPredicateAnd() {
+
+        Predicate<String> letterLengthLimit = s -> s.length() > 5;
+        Predicate<String> letterStartWith = s -> s.startsWith("gate");
+
+        MICRO_SERVICE.stream().filter(
+                letterLengthLimit.and(letterStartWith)
+        ).forEach(System.out::println);
+    }
+
+    /**
+     * <h2>or 等同于我们的逻辑或 ||, 多个条件主要一个满足即可</h2>
+     * */
+    @Test
+    public void testPredicateOr() {
+
+        Predicate<String> letterLengthLimit = s -> s.length() > 5;
+        Predicate<String> letterStartWith = s -> s.startsWith("gate");
+
+        MICRO_SERVICE.stream().filter(
+                letterLengthLimit.or(letterStartWith)
+        ).forEach(System.out::println);
+    }
+
+    /**
+     * <h2>negate 等同于我们的逻辑非 !</h2>
+     * */
+    @Test
+    public void testPredicateNegate() {
+
+        Predicate<String> letterStartWith = s -> s.startsWith("gate");
+        MICRO_SERVICE.stream().filter(letterStartWith.negate()).forEach(System.out::println);
+    }
+
+    /**
+     * <h2>isEqual 类似于 equals(), 区别在于: 先判断对象是否为 NULL,
+     * 不为 NULL 再使用 equals 进行比较</h2>
+     * */
+    @Test
+    public void testPredicateIsEqual() {
+
+        Predicate<String> equalGateway = s -> Predicate.isEqual("gateway").test(s);
+        MICRO_SERVICE.stream().filter(equalGateway).forEach(System.out::println);
+    }
 }
